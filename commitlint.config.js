a8f4ba6bc1f9a439d3db3e98c90459822928d4c1
Mike at Bendyline <mike@bendyline.com>
2026-07-31 08:04:09 -0700

    fix: Update lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'body-max-line-length': [2, 'always', 1024],
     'header-max-length': [2, 'always', 1024],
     'subject-case': [0],
   },
